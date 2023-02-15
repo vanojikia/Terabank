c88640f701e110fd16afc85e861440e3625eb66a
ivane.jikia <ivane.jikia@IJIKIA>
2023-02-15 14:51:35 +0400

    Add project files.

---
+++ Terabank/Program.cs
@@ -0,0 +1,29 @@
+﻿/* pirveli sashinao davaleba
+ * 
+ */
+
+class Homework
+{
+    static void Main(string[] args)
+    {
+        //misalmeba
+        Console.WriteLine("Hello Mr. Archil");
+        Console.WriteLine("How are you?");
+
+        //about
+        Console.WriteLine("Gtxovt ixilot chemi sashinao davaleba #1");
+        Console.WriteLine("...");
+
+        //comment
+        Console.WriteLine("warmatebebi");
+        Console.WriteLine("...");
+    }
+
+    /// <summary>
+    /// es kodi ar igebs monawileobas
+    /// </summary>
+    static void regularMethod()
+    {
+        Console.WriteLine("We Are Junior Developers");
+    }
+}
