57da3115d01eb10245e42933daef664862caffd3
vanojikia <jikiavano@gmail.com>
2023-02-22 17:23:05 +0400

    New Homework

--- Terabank/Program.cs
+++ Terabank/Program.cs
@@ -2,6 +2,13 @@
 {
     static void Main(string[] args)
     {
-    
+        Console.WriteLine(Numbers(5, 6));
+    }
+
+    //1. შევქმნათ მეთოდი, რომელსაც გადავცემთ ორ სრულ რიცხვს. მეთოდმა უნდა დააბრუნოს ორი რიცხვის ჯამი. გამოვიყვანოთ ეს ჯამი შესვლის წერტილის მეთოდის მეშვეობით კონსოლში.
+    static int Numbers(int x, int y)
+    {
+        int sum = x + y;
+        return sum;
     }
 }
