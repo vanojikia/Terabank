b70c3d9edd275cc3234cddea1e1513d29a1df5a7
vanojikia <jikiavano@gmail.com>
2023-02-22 14:39:46 +0400

    Update

--- Terabank/Program.cs
+++ Terabank/Program.cs
@@ -1,29 +1,7 @@
-﻿/* pirveli sashinao davaleba
- * 
- */
-
-class Homework
+﻿class Homework3
 {
     static void Main(string[] args)
     {
-        //misalmeba
-        Console.WriteLine("Hello Mr. Archil");
-        Console.WriteLine("How are you?");
-
-        //about
-        Console.WriteLine("Gtxovt ixilot chemi sashinao davaleba #1");
-        Console.WriteLine("...");
-
-        //comment
-        Console.WriteLine("warmatebebi");
-        Console.WriteLine("...");
-    }
-
-    /// <summary>
-    /// es kodi ar igebs monawileobas
-    /// </summary>
-    static void regularMethod()
-    {
-        Console.WriteLine("We Are Junior Developers");
+    
     }
 }
